83bf714b9a3dc2b45ac7c1c063fc1bfbe05e3dbc
Promise Orung <promiseorung@gmail.com>
2022-07-14 11:47:02 +0000

    Attempt question 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+  let sum = 0;
+  for(let array of arrayOfNumbers){
+    sum+= array
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ let array = [];
+  let count;
+  for(evenNumbers of arrayOfNumbers) {
+    if(evenNumbers %2 ==0) {
+      count = array.push(evenNumbers)
+    }
+  }
+  return array.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
